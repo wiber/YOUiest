a874df2ce4ceb2b0d93dd124f032234aff2d3230
wiber <eliasmoosman@gmail.com>
2011-08-30 03:24:28 +0000

    github api

--- youiest.js
+++ youiest.js
@@ -7,6 +7,6 @@ document.head.appendChild(script);
 
 var script = document.createElement( 'script' );
 script.type = 'text/sjs';
-script.text = "require('onlyyou');";
+script.text = "require('github:wiber/youiest/master/youiest.js";
 document.head.appendChild(script);
 //<script src="http://code.onilabs.com/apollo/0.12/oni-apollo.js "></script> <script type="text/sjs">require('onlyyou'); </script>
